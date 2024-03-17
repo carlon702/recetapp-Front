b2a0c8cf1bf5f56b2c57cfed8a02226fafb156d1
FranciscoVaninetti <francisco.vaninetti@gmail.com>
2024-03-17 18:24:23 -0300

    Added base folder structure and WIP base layout for pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {" "}
+        <header style={{ backgroundColor: "lightblue", paddingBottom: "1rem" }}>
+          <p>Header</p>
+        </header>
+        {children}
+        <footer style={{ backgroundColor: "ghostwhite", paddingTop: "1rem" }}>
+          <p>Footer</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
